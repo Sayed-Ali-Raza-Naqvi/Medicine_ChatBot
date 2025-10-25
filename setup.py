6624928b4f79ff0b009a8d2f39f58f7a39600aba
Syed Ali Raza Naqvi <alisyednaqvi1512@gmail.com>
2025-10-25 15:22:49 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medicine_ChatBot",
+    version="0.1.0",
+    author="Syed Ali Raza Naqvi",
+    author_email="alisyednaqvi1512@gmail.com",
+    description="A chatbot application for medical inquiries using LangChain and Pinecone.",
+    packages=find_packages(),
+    install_requires=[],
+)
